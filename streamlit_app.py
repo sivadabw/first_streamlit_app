32f283d912f4b9fbf288222ea5a4a2289669444d
sivadabw <155433514+sivadabw@users.noreply.github.com>
2024-01-05 13:09:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,6 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+# streamlit.text(fruityvice_response) Change it to add json
+streamlit.text(fruityvice_response.json())
+
